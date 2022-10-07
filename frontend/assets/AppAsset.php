7faaee6e5ad1282d1d365dfca946c844b4fe8b54
Dev <devp2101@gmail.com>
2022-10-07 11:24:15 +0530

    - new design files are integrated

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -13,8 +13,27 @@ class AppAsset extends AssetBundle
     public $baseUrl = '@web';
     public $css = [
         'css/site.css',
+        'css/bootstrap.min.css',
+        'css/font.awesome.css',
+        'css/pe-icon-7-stroke.css',
+        'css/animate.min.css',
+        'css/swiper-bundle.min.css',
+        'css/venobox.css',
+        'css/jquery-ui.min.css',
+        'css/style.css',
+        'https://cdn.jsdelivr.net/npm/pixeden-stroke-7-icon@1.2.3/pe-icon-7-stroke/dist/pe-icon-7-stroke.min.css',
+        'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'
     ];
     public $js = [
+        'js/vendor/bootstrap.bundle.min.js',
+        'js/vendor/modernizr-3.11.2.min.js',
+        'js/plugins/jquery.countdown.min.js',
+        'js/plugins/swiper-bundle.min.js',
+        'js/plugins/scrollUp.js',
+        'js/plugins/venobox.min.js',
+        'js/plugins/jquery-ui.min.js',
+        'js/plugins/mailchimp-ajax.js',
+        'js/main.js'
     ];
     public $depends = [
         'yii\web\YiiAsset',
