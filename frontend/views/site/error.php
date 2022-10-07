8621fab9a2e38bb38abb00a2049f505032de18b1
Dev <devp2101@gmail.com>
2022-10-07 14:18:28 +0530

    design change

--- frontend/views/site/error.php
+++ frontend/views/site/error.php
@@ -9,19 +9,15 @@ use yii\helpers\Html;
 
 $this->title = $name;
 ?>
-<div class="site-error">
-
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <div class="alert alert-danger">
-        <?= nl2br(Html::encode($message)) ?>
+<div class="section-404 section" data-bg-image="<?= Yii::getAlias('@web/images/404/404-white.png')?>">
+    <div class="container">
+        <div class="content-404">
+            <h2 class="title"><?= Html::encode($this->title) ?></h2>
+            <h3 class="sub-title"><?= nl2br(Html::encode($message)) ?></h3>
+            <p>You could either go back or go to homepage</p>
+            <div class="buttons">
+                <a class="btn btn-dark btn-outline-hover-dark" href="<?= \yii\helpers\Url::toRoute('/')?>">Homepage</a>
+            </div>
+        </div>
     </div>
-
-    <p>
-        The above error occurred while the Web server was processing your request.
-    </p>
-    <p>
-        Please contact us if you think this is a server error. Thank you.
-    </p>
-
 </div>
