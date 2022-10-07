b2090285626febe9eddcd3b41a2dbeb3b9683b0f
Dev <devp2101@gmail.com>
2022-10-07 15:27:38 +0530

    -bug fixes in blank.php -login and signup page new design set -new css code is added

--- frontend/views/site/login.php
+++ frontend/views/site/login.php
@@ -3,39 +3,55 @@
 /** @var yii\web\View $this */
 /** @var yii\bootstrap5\ActiveForm $form */
 /** @var \common\models\LoginForm $model */
+/** @var \frontend\models\SignupForm $register */
 
 use yii\bootstrap5\Html;
 use yii\bootstrap5\ActiveForm;
+use yii\helpers\Url;
 
 $this->title = 'Login';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="site-login">
-    <h1><?= Html::encode($this->title) ?></h1>
+<!-- login area start -->
+<div class="login-register-area pt-100px pb-100px">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-7 col-md-12 ml-auto mr-auto">
+                <div class="login-register-wrapper">
+                    <div class="login-register-tab-list nav">
+                        <a class="active"  href="<?= Url::toRoute('site/login')?>">
+                            <h4>login</h4>
+                        </a>
+                        <a  href="<?= Url::toRoute('site/signup')?>">
+                            <h4>register</h4>
+                        </a>
+                    </div>
+                    <div class="tab-content">
+                        <div id="lg1" class="tab-pane active">
+                            <div class="login-form-container">
+                                <div class="login-register-form">
+                                    <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
 
-    <p>Please fill out the following fields to login:</p>
+                                    <?= $form->field($model, 'username')->textInput(['placeholder' => 'Username'])->label(false) ?>
 
-    <div class="row">
-        <div class="col-lg-5">
-            <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
+                                    <?= $form->field($model, 'password')->passwordInput(['placeholder'=>'Password'])->label(false)?>
 
-                <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
-
-                <?= $form->field($model, 'password')->passwordInput() ?>
-
-                <?= $form->field($model, 'rememberMe')->checkbox() ?>
-
-                <div class="my-1 mx-0" style="color:#999;">
-                    If you forgot your password you can <?= Html::a('reset it', ['site/request-password-reset']) ?>.
-                    <br>
-                    Need new verification email? <?= Html::a('Resend', ['site/resend-verification-email']) ?>
+                                     <div class="button-box">
+                                            <div class="login-toggle-btn">
+                                                <input type="checkbox" name="rememberMe" />
+                                                <a class="flote-none" href="javascript:void(0)">Remember me</a>
+                                                <a href="#">Forgot Password?</a>
+                                            </div>
+                                         <?= Html::submitButton('Login', ['class' => 'btn btn-sm btn-secondary', 'name' => 'login-button']) ?>
+                                        </div>
+                                    <?php ActiveForm::end(); ?>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-
-                <div class="form-group">
-                    <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
-                </div>
-
-            <?php ActiveForm::end(); ?>
+            </div>
         </div>
     </div>
 </div>
+<!-- login area end -->
--- frontend/views/site/signup.php
+++ frontend/views/site/signup.php
@@ -6,30 +6,47 @@
 
 use yii\bootstrap5\Html;
 use yii\bootstrap5\ActiveForm;
+use yii\helpers\Url;
 
 $this->title = 'Signup';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="site-signup">
-    <h1><?= Html::encode($this->title) ?></h1>
+<!-- login area start -->
+<div class="login-register-area pt-100px pb-100px">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-7 col-md-12 ml-auto mr-auto">
+                <div class="login-register-wrapper">
+                    <div class="login-register-tab-list nav">
+                        <a href="<?= Url::toRoute('site/login')?>">
+                            <h4>login</h4>
+                        </a>
+                        <a class="active" href="<?= Url::toRoute('site/signup')?>">
+                            <h4>register</h4>
+                        </a>
+                    </div>
+                    <div class="tab-content">
+                        <div id="lg2" class="tab-pane active">
+                            <div class="login-form-container">
+                                <div class="login-register-form">
+                                    <?php $form = ActiveForm::begin(['id' => 'form-signup']); ?>
 
-    <p>Please fill out the following fields to signup:</p>
+                                    <?= $form->field($model, 'username')->textInput(['placeholder' =>'Username'])->label(false) ?>
 
-    <div class="row">
-        <div class="col-lg-5">
-            <?php $form = ActiveForm::begin(['id' => 'form-signup']); ?>
+                                    <?= $form->field($model, 'email')->textInput(['placeholder' => 'Email'])->label(false)?>
 
-                <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
-
-                <?= $form->field($model, 'email') ?>
-
-                <?= $form->field($model, 'password')->passwordInput() ?>
-
-                <div class="form-group">
-                    <?= Html::submitButton('Signup', ['class' => 'btn btn-primary', 'name' => 'signup-button']) ?>
+                                    <?= $form->field($model, 'password')->passwordInput(['placeholder' => 'Password'])->label(false) ?>
+                                    <div class="button-box">
+                                        <?= Html::submitButton('Register', ['class' => 'btn btn-secondary btn-sm']) ?>
+                                    </div>
+                                    <?php ActiveForm::end(); ?>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-
-            <?php ActiveForm::end(); ?>
+            </div>
         </div>
     </div>
 </div>
+<!-- login area end -->
