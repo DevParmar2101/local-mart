52370d50430591e4804ff3967d519fc55ceb5d16
Dev <devp2101@gmail.com>
2022-10-07 15:34:50 +0530

    -request-password-reset design change

--- frontend/views/site/requestPasswordResetToken.php
+++ frontend/views/site/requestPasswordResetToken.php
@@ -6,26 +6,44 @@
 
 use yii\bootstrap5\Html;
 use yii\bootstrap5\ActiveForm;
+use yii\helpers\Url;
 
 $this->title = 'Request password reset';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="site-request-password-reset">
-    <h1><?= Html::encode($this->title) ?></h1>
+<!-- login area start -->
+<div class="login-register-area pt-100px pb-100px">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-7 col-md-12 ml-auto mr-auto">
+                <div class="login-register-wrapper">
+                    <div class="login-register-tab-list nav">
+                        <a class="active"  href="<?= Url::toRoute('site/request-password-reset')?>">
+                            <h4>Password Reset</h4>
+                        </a>
+                        <a  href="<?= Url::toRoute('site/login')?>">
+                            <h4>Login</h4>
+                        </a>
+                    </div>
+                    <div class="tab-content">
+                        <div id="lg1" class="tab-pane active">
+                            <div class="login-form-container">
+                                <div class="login-register-form">
+                                    <?php $form = ActiveForm::begin(['id' => 'request-password-reset-form']); ?>
 
-    <p>Please fill out your email. A link to reset password will be sent there.</p>
+                                    <?= $form->field($model, 'email')->textInput(['placeholder' => 'Email Address'])->label(false) ?>
 
-    <div class="row">
-        <div class="col-lg-5">
-            <?php $form = ActiveForm::begin(['id' => 'request-password-reset-form']); ?>
-
-                <?= $form->field($model, 'email')->textInput(['autofocus' => true]) ?>
-
-                <div class="form-group">
-                    <?= Html::submitButton('Send', ['class' => 'btn btn-primary']) ?>
+                                    <div class="button-box">
+                                        <?= Html::submitButton('Send', ['class' => 'btn btn-sm btn-secondary', 'name' => 'login-button']) ?>
+                                    </div>
+                                    <?php ActiveForm::end(); ?>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-
-            <?php ActiveForm::end(); ?>
+            </div>
         </div>
     </div>
 </div>
+<!-- login area end -->
