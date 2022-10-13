92547d00d3eacc885554c0b6746fec19723f94a5
Dev <devp2101@gmail.com>
2022-10-13 12:06:08 +0530

    -admin-url-manager is added in this __autocomplete.php file -admin-url-manager route is added -bug fix

--- common/config/main.php
+++ common/config/main.php
@@ -9,5 +9,11 @@ return [
         'cache' => [
             'class' => \yii\caching\FileCache::class,
         ],
+        'urlManagerAdmin' => [
+            'class' => \yii\web\UrlManager::class,
+            'baseUrl' => '/admin/',
+            'enablePrettyUrl' => true,
+            'showScriptName' => false
+        ],
     ],
 ];
--- common/models/BaseActiveRecord.php
+++ common/models/BaseActiveRecord.php
@@ -2,6 +2,10 @@
 namespace common\models;
 
 use yii\db\ActiveRecord;
+use yii\helpers\Html;
+use yii\helpers\Inflector;
+use yii\helpers\StringHelper;
+use yii\web\UploadedFile;
 
 class BaseActiveRecord extends ActiveRecord
 {
@@ -25,5 +29,64 @@ class BaseActiveRecord extends ActiveRecord
             return $array[$status];
         }
         return $array;
+    }
+
+    /**
+     * @param $image UploadedFile
+     *
+     * @return string
+     * @throws \yii\base\Exception
+     */
+    public function getImageName(UploadedFile $image): string
+    {
+        return Inflector::slug($image->baseName).\Yii::$app->security->generateRandomString(16).'.'.$image->extension;
+    }
+    /**
+     * @param $name
+     * @return false|string
+     */
+    public static function getPath($name=null){
+        $path = \Yii::getAlias('@backend/web/'.self::getPathPrefix());
+        if($name){
+            return $path.$name;
+        }
+        return $path;
+    }
+
+    /**
+     * @param $name
+     * @return string
+     */
+    public static function getPathPrefix($name=null): string
+    {
+        $path = '/uploads/'.Inflector::camel2id(StringHelper::basename(get_called_class()), '_').'/';
+        if($name){
+            return $path.$name;
+        }
+        return $path;
+    }
+
+    /**
+     * @param $name
+     * @param string $alt
+     * @param string $class
+     * @param $data_speed
+     * @return string|void
+     */
+    public function getImage($name, string $alt='', string $class='',$data_speed = null){
+        if($this->{$name}){
+            return Html::img($this->getImageUrl($name),
+                ['class' => $class,'alt' =>$alt,'data-speed' => $data_speed]);
+        }
+    }
+
+    /**
+     * @param $name
+     * @return string|void
+     */
+    public function getImageUrl($name){
+        if($this->{$name}){
+            return \Yii::$app->urlManagerAdmin->createAbsoluteUrl(self::getPathPrefix($this->{$name}));
+        }
     }
 }
