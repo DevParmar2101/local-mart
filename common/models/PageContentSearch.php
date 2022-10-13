25b3866edb2bf9f61495f5d8932f33025ca3ee6c
Dev <devp2101@gmail.com>
2022-10-13 13:47:41 +0530

    -page-content-search model is generated in common/models

---
+++ common/models/PageContentSearch.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace common\models;
+
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+use common\models\PageContent;
+
+/**
+ * PageContentSearch represents the model behind the search form of `common\models\PageContent`.
+ */
+class PageContentSearch extends PageContent
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['id', 'use_for', 'order_by', 'status', 'user_id'], 'integer'],
+            [['title', 'sub_title', 'content'], 'safe'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function scenarios()
+    {
+        // bypass scenarios() implementation in the parent class
+        return Model::scenarios();
+    }
+
+    /**
+     * Creates data provider instance with search query applied
+     *
+     * @param array $params
+     *
+     * @return ActiveDataProvider
+     */
+    public function search($params)
+    {
+        $query = PageContent::find();
+
+        // add conditions that should always apply here
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        $this->load($params);
+
+        if (!$this->validate()) {
+            // uncomment the following line if you do not want to return any records when validation fails
+            // $query->where('0=1');
+            return $dataProvider;
+        }
+
+        // grid filtering conditions
+        $query->andFilterWhere([
+            'id' => $this->id,
+            'use_for' => $this->use_for,
+            'order_by' => $this->order_by,
+            'status' => $this->status,
+            'user_id' => $this->user_id,
+        ]);
+
+        $query->andFilterWhere(['like', 'title', $this->title])
+            ->andFilterWhere(['like', 'sub_title', $this->sub_title])
+            ->andFilterWhere(['like', 'content', $this->content]);
+
+        return $dataProvider;
+    }
+}
