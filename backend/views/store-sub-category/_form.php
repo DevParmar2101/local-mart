514d3c1bf61b4b7dc69eeedbb4b4ef73808d7dc6
Dev <devp2101@gmail.com>
2022-10-13 19:12:33 +0530

    -controller bug fix

--- backend/views/store-sub-category/_form.php
+++ backend/views/store-sub-category/_form.php
@@ -5,7 +5,7 @@ use yii\bootstrap5\ActiveForm;
 
 /** @var yii\web\View $this */
 /** @var common\models\StoreSubCategory $model */
-/** @var \yii\bootstrap5\ActiveForm $form */
+/** @var ActiveForm $form */
 ?>
 
 <div class="card border-top border-0 border-4 border-white">
@@ -17,25 +17,30 @@ use yii\bootstrap5\ActiveForm;
             <h5 class="mb-0 text-white"><?= Html::encode($this->title)?></h5>
         </div>
         <hr>
-    <?php $form = ActiveForm::begin(['id' => 'store-sub-category-form']); ?>
+        <?php $form = ActiveForm::begin(['id' => 'store-sub-category-form']); ?>
 
-    <?= $form->field($model, 'category_name')->textInput() ?>
+        <div class="row">
+            <div class="col-md-6 col-12">
+                <?= $form->field($model, 'category_name')->textInput() ?>
+            </div>
+            <div class="col-md-6 col-12">
+                <?= $form->field($model, 'sub_category')->textInput() ?>
+            </div>
+        </div>
 
-    <?= $form->field($model, 'sub_category')->textInput(['maxlength' => true]) ?>
+        <div class="row">
+            <div class="col-md-6 col-12">
+                <?= $form->field($model, 'status')->widget(\kartik\select2\Select2::class,[
+                        'data' => $model->getStatus()
+                ]) ?>
+            </div>
+        </div>
 
-    <?= $form->field($model, 'user_id')->textInput() ?>
+        <div class="form-group">
+            <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+        </div>
 
-    <?= $form->field($model, 'status')->textInput() ?>
-
-    <?= $form->field($model, 'created_at')->textInput() ?>
-
-    <?= $form->field($model, 'updated_at')->textInput() ?>
-
-    <div class="form-group">
-        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
-    </div>
-
-    <?php ActiveForm::end(); ?>
+        <?php ActiveForm::end(); ?>
     </div>
 </div>
 
