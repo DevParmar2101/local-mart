80bcabd55252470189a310a9ac0de6072a6078e0
Dev <devp2101@gmail.com>
2022-10-13 15:52:56 +0530

    -bug fixes

--- frontend/views/page/index.php
+++ frontend/views/page/index.php
@@ -5,9 +5,16 @@ use common\models\PageContent;
 /** @var $model PageContent*/
 
 ?>
-
-<?= $model->title?>
-<br>
-<?= $model->sub_title?>
-<br>
-<?= $model->content?>
+<div class="container-lg h-100">
+    <div class="px-lg-4 py-4 py-sm-5">
+        <h1 class="text-center after-border">
+            <?= $model->title?>
+        </h1>
+        <div class="justify-content-center btn-group text-center d-block">
+            <h4 class="content-font page-sub-title"><?= $model->sub_title?></h4>
+        </div>
+        <div class="pt-4">
+            <?= $model->content?>
+        </div>
+    </div>
+</div>
