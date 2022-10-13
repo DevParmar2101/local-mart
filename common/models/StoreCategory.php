aa3d33d4636916d096d42342c7bd74578c56d9d9
Dev <devp2101@gmail.com>
2022-10-13 18:07:13 +0530

    -store-category model is generated

---
+++ common/models/StoreCategory.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "store_category".
+ *
+ * @property int $id
+ * @property string|null $category_name
+ * @property int|null $user_id
+ * @property int|null $status
+ * @property string|null $created_at
+ * @property string|null $updated_at
+ *
+ * @property UserStore[] $userStores
+ */
+class StoreCategory extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'store_category';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['user_id', 'status'], 'integer'],
+            [['created_at', 'updated_at'], 'safe'],
+            [['category_name'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'category_name' => 'Category Name',
+            'user_id' => 'User ID',
+            'status' => 'Status',
+            'created_at' => 'Created At',
+            'updated_at' => 'Updated At',
+        ];
+    }
+
+    /**
+     * Gets query for [[UserStores]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getUserStores()
+    {
+        return $this->hasMany(UserStore::class, ['store_category' => 'id']);
+    }
+}
