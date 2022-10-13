23b2886a0258ebacc6ac38b1a372319fe60c42f2
Dev <devp2101@gmail.com>
2022-10-13 17:57:42 +0530

    -migration file to create user_store_table is added -and query is also added in user_store_table

---
+++ console/migrations/m221013_121746_create_user_store_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%user_store}}`.
+ */
+class m221013_121746_create_user_store_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%user_store}}', [
+            'id' => $this->primaryKey(),
+            'first_name' => $this->string(255),
+            'last_name' => $this->string(255),
+            'contact_number' => $this->string(15),
+            'is_number_verified' => $this->tinyInteger(2),
+            'user_id' => $this->integer(11),
+            'store_name' => $this->string(255),
+            'state' => $this->integer(11),
+            'city' => $this->integer(11),
+            'address' => $this->text(),
+            'zip_code' => $this->integer(10),
+            'status' => $this->tinyInteger(2),
+            'store_category' => $this->integer(11),
+            'purchase_type' => $this->tinyInteger(2),
+            'created_at' => $this->dateTime(),
+            'updated_at' => $this->dateTime(),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%user_store}}');
+    }
+}
