a307eadf5266de0f7a1917d010d2a54d7ec5f8ec
Dev <devp2101@gmail.com>
2022-10-13 18:18:43 +0530

    - index page design change

--- backend/views/store-category/index.php
+++ backend/views/store-category/index.php
@@ -13,36 +13,31 @@ use yii\grid\GridView;
 $this->title = 'Store Categories';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="store-category-index">
+<div class="card">
+    <div class="card-body">
+        <div class="d-lg-flex align-items-center mb-4 gap-3">
+            <div class="ms-auto">
+                <?= Html::a('<i class="bx bxs-plus-square"></i>Create Store Category', ['create'], ['class' => 'btn btn-light radius-30 mt-2 mt-lg-0']) ?>
+            </div>
+        </div>
+        <?= GridView::widget([
+            'dataProvider' => $dataProvider,
+            'filterModel' => $searchModel,
+            'columns' => [
+                ['class' => 'yii\grid\SerialColumn'],
 
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <p>
-        <?= Html::a('Create Store Category', ['create'], ['class' => 'btn btn-success']) ?>
-    </p>
-
-    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
-
-    <?= GridView::widget([
-        'dataProvider' => $dataProvider,
-        'filterModel' => $searchModel,
-        'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
-
-            'id',
-            'category_name',
-            'user_id',
-            'status',
-            'created_at',
-            //'updated_at',
-            [
-                'class' => ActionColumn::className(),
-                'urlCreator' => function ($action, StoreCategory $model, $key, $index, $column) {
-                    return Url::toRoute([$action, 'id' => $model->id]);
-                 }
+                'id',
+                'category_name',
+                'user_id',
+                'status',
+                'created_at',
+                [
+                    'class' => ActionColumn::className(),
+                    'urlCreator' => function ($action, StoreCategory $model, $key, $index, $column) {
+                        return Url::toRoute([$action, 'id' => $model->id]);
+                    }
+                ],
             ],
-        ],
-    ]); ?>
-
-
+        ]); ?>
+    </div>
 </div>
