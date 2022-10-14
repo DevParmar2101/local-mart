5af91131478c7f4c17c3fee54ac9536545a8890f
Dev <devp2101@gmail.com>
2022-10-14 15:07:54 +0530

    in SettingsController.php controller file actionIndex fully refactor

--- backend/controllers/SettingsController.php
+++ backend/controllers/SettingsController.php
@@ -4,9 +4,11 @@ namespace backend\controllers;
 
 use common\models\Settings;
 use common\models\SettingsSearch;
+use yii\helpers\FileHelper;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
+use yii\web\UploadedFile;
 
 /**
  * SettingsController implements the CRUD actions for Settings model.
@@ -32,14 +34,91 @@ class SettingsController extends Controller
     }
 
     /**
-     * Lists all Settings models.
-     *
-     * @return string
+     * @return string|\yii\web\Response
+     * @throws \yii\base\Exception
      */
     public function actionIndex()
     {
-        $model = new Settings();
+        $model = Settings::findOne(['id' => 1]);
+        if (!$model){
+            $model = new Settings();
+        }
+        $path = Settings::getPath();
+        if (!is_dir($path)) {
+            FileHelper::createDirectory($path,$mode = 0777, $recursive = true);
+        }
+        $old_logo_dark = null;
+        $old_logo_light = null;
+        $old_logo_transparent = null;
+        $old_favicon = null;
 
+        if ($model->logo_dark) {
+            $old_logo_dark = $model->logo_dark;
+        }
+
+        if ($model->logo_light) {
+            $old_logo_light = $model->logo_light;
+        }
+
+        if ($model->logo_transparent) {
+            $old_logo_transparent = $model->logo_transparent;
+        }
+
+        if ($model->favicon) {
+            $old_favicon = $model->favicon;
+        }
+
+        if ($this->request->isPost && $model->load($this->request->post())) {
+            $logo_dark = UploadedFile::getInstance($model,'logo_dark');
+            $logo_light = UploadedFile::getInstance($model,'logo_light');
+            $logo_transparent = UploadedFile::getInstance($model,'logo_transparent');
+            $favicon = UploadedFile::getInstance($model,'favicon');
+
+            if ($logo_dark) {
+                $model->logo_dark = $model->getImageName($logo_dark);
+            }else{
+                $model->logo_dark = $old_logo_dark;
+            }
+
+            if ($logo_light) {
+                $model->logo_light = $model->getImageName($logo_light);
+            }else{
+                $model->logo_light = $old_logo_light;
+            }
+
+            if ($logo_transparent) {
+                $model->logo_transparent = $model->getImageName($logo_transparent);
+            }else{
+                $model->logo_transparent = $old_logo_transparent;
+            }
+
+            if ($favicon) {
+                $model->favicon = $model->getImageName($favicon);
+            }else{
+                $model->favicon = $old_favicon;
+            }
+
+            if ($model->save()){
+                if ($logo_dark) {
+                    $logo_dark->saveAs(Settings::getPath($model->logo_dark));
+                }
+
+                if ($logo_light) {
+                    $logo_light->saveAs(Settings::getPath($model->logo_light));
+                }
+
+                if ($logo_transparent) {
+                    $logo_transparent->saveAs(Settings::getPath($model->logo_transparent));
+                }
+
+                if ($favicon) {
+                    $favicon->saveAs(Settings::getPath($model->favicon));
+                }
+
+                \Yii::$app->session->getFlash('success','Content updated successfully !');
+                return $this->redirect(['index']);
+            }
+        }
         return $this->render('index',[
             'model' => $model
         ]);
