c751e0aaf4861e82978d2c2ff89383e7c5414b1d
Dev <devp2101@gmail.com>
2022-10-14 15:07:22 +0530

    -public strings are added in this GlobalProperty.php file

--- common/components/GlobalProperty.php
+++ common/components/GlobalProperty.php
@@ -6,5 +6,69 @@ use yii\base\Component;
 
 class GlobalProperty extends Component
 {
+    /**
+     * @var $name string
+     */
+    public string $name = 'LocalMart';
+
+    /**
+     * @var $email_host string
+     */
+    public string $email_host = '';
+
+    /**
+     * @var $email_username string
+     */
+    public string $email_username = '';
+
+    /**
+     * @var $email_password string
+     */
+    public string $email_password = '';
+
+    /**
+     * @var $email_port string
+     */
+    public string $email_port = '';
+
+    /**
+     * @var $twilio_account_sid string
+     */
+    public string $twilio_account_sid = '';
+
+    /**
+     * @var $twilio_auth_token string
+     */
+    public string $twilio_auth_token = '';
+
+    /**
+     * @var $twilio_service_sid string
+     */
+    public string $twilio_service_sid = '';
+
+    /**
+     * @var $twilio_phone_number string
+     */
+    public string $twilio_phone_number = '';
+
+    /**
+     * @var $logo_dark string
+     */
+    public string $logo_dark = '';
+
+    /**
+     * @var $logo_light string
+     */
+    public string $logo_light = '';
+
+    /**
+     * @var $logo_transparent string
+     */
+    public string $logo_transparent = '';
+
+    /**
+     * @var $favicon string
+     */
+    public string $favicon = '';
 
 }
