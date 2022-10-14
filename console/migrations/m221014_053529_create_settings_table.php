ffed89b60fd2f95348e88fbdc1a781038926a5ad
Dev <devp2101@gmail.com>
2022-10-14 11:13:44 +0530

    -query for the creation of settings table

--- console/migrations/m221014_053529_create_settings_table.php
+++ console/migrations/m221014_053529_create_settings_table.php
@@ -14,6 +14,20 @@ class m221014_053529_create_settings_table extends Migration
     {
         $this->createTable('{{%settings}}', [
             'id' => $this->primaryKey(),
+            'app_name' =>               $this->string(50),
+            'email_host' =>             $this->string(100),
+            'email_username' =>         $this->string(100),
+            'email_password' =>         $this->string(225),
+            'email_port' =>             $this->string(10),
+            'email_encryption' =>       $this->string(10),
+            'twilio_account_sid' =>     $this->string(255),
+            'twilio_auth_token' =>      $this->string(255),
+            'twilio_service_sid' =>     $this->string(255),
+            'twilio_phone_number' =>    $this->string(15),
+            'logo_dark' =>              $this->string(255),
+            'logo_light' =>             $this->string(255),
+            'logo_transparent' =>       $this->string(255),
+            'favicon' =>                $this->string(255),
         ]);
     }
 
