78062b7b98e3d92a7d5cbea60b38157f4235e69d
Dev <devp2101@gmail.com>
2022-10-14 16:24:02 +0530

    -States.php model file generated in common/models folder

---
+++ common/models/States.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "states".
+ *
+ * @property int $id
+ * @property string|null $name
+ *
+ * @property Cities[] $cities
+ */
+class States extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'states';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['name'], 'string', 'max' => 50],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+        ];
+    }
+
+    /**
+     * Gets query for [[Cities]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getCities()
+    {
+        return $this->hasMany(Cities::class, ['state_id' => 'id']);
+    }
+}
