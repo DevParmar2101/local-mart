60ce0f1df996468ca2e7b14c7ba79d9366ee7284
Dev <devp2101@gmail.com>
2022-10-14 10:21:20 +0530

    -bug fixes

--- backend/controllers/StoreSubCategoryController.php
+++ backend/controllers/StoreSubCategoryController.php
@@ -70,7 +70,9 @@ class StoreSubCategoryController extends Controller
         $model = new StoreSubCategory();
 
         if ($this->request->isPost) {
-            if ($model->load($this->request->post()) && $model->save()) {
+            if ($model->load($this->request->post())) {
+                $model->user_id = \Yii::$app->user->identity->id;
+                $model->save();
                 return $this->redirect(['view', 'id' => $model->id]);
             }
         } else {
