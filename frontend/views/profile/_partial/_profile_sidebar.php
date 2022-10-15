bcf57047be794eaf67c1d519551f338766d5c940
Dev <devp2101@gmail.com>
2022-10-15 23:33:41 +0530

    - ProfileController is also added

--- frontend/views/profile/_partial/_profile_sidebar.php
+++ frontend/views/profile/_partial/_profile_sidebar.php
@@ -1,18 +1,20 @@
 <?php
 
 use yii\helpers\Html;
+use yii\helpers\Url;
+
 ?>
 <div class="col-sm-12 col-md-3 col-lg-3">
     <!-- Nav tabs -->
     <div class="dashboard_tab_button" data-aos="fade-up" data-aos-delay="0">
         <ul role="tablist" class="nav flex-column dashboard-list">
-            <li><a href="#dashboard" data-bs-toggle="tab" class="nav-link active">Dashboard</a></li>
-            <li> <a href="#orders" data-bs-toggle="tab" class="nav-link">Orders</a></li>
-            <li><a href="#downloads" data-bs-toggle="tab" class="nav-link">Downloads</a></li>
-            <li><a href="#address" data-bs-toggle="tab" class="nav-link">Addresses</a></li>
-            <li><a href="#account-details" data-bs-toggle="tab" class="nav-link">Account details</a>
+            <li><a href="<?= Url::toRoute('profile/account')?>" data-bs-toggle="tab" class="nav-link active">Account Details</a></li>
+            <li> <a href="<?= Url::toRoute('profile/wishlist')?>" data-bs-toggle="tab" class="nav-link"> Wishlist</a></li>
+            <li> <a href="<?= Url::toRoute('profile/order')?>" data-bs-toggle="tab" class="nav-link">My Order</a></li>
+            <li>
+                <?= Html::a('Logout',['site/logout'],['data' => ['method' => 'post'],'class' => 'nav-link'])?>
             </li>
-            <li><a href="login.html" class="nav-link">logout</a></li>
+
         </ul>
     </div>
 </div>
