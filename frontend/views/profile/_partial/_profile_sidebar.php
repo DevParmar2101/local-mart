b533a902edd7a6b3a4377ce51c77a3527dfe41af
Dev <devp2101@gmail.com>
2022-10-15 23:23:32 +0530

    - _profile_sidebar.php partial view folder and _partial view file is added in profile/_partial folder

---
+++ frontend/views/profile/_partial/_profile_sidebar.php
@@ -0,0 +1,18 @@
+<?php
+
+use yii\helpers\Html;
+?>
+<div class="col-sm-12 col-md-3 col-lg-3">
+    <!-- Nav tabs -->
+    <div class="dashboard_tab_button" data-aos="fade-up" data-aos-delay="0">
+        <ul role="tablist" class="nav flex-column dashboard-list">
+            <li><a href="#dashboard" data-bs-toggle="tab" class="nav-link active">Dashboard</a></li>
+            <li> <a href="#orders" data-bs-toggle="tab" class="nav-link">Orders</a></li>
+            <li><a href="#downloads" data-bs-toggle="tab" class="nav-link">Downloads</a></li>
+            <li><a href="#address" data-bs-toggle="tab" class="nav-link">Addresses</a></li>
+            <li><a href="#account-details" data-bs-toggle="tab" class="nav-link">Account details</a>
+            </li>
+            <li><a href="login.html" class="nav-link">logout</a></li>
+        </ul>
+    </div>
+</div>
