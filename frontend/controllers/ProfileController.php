526b66a6ff8aa279a4b1db14f1b6f145d8c457aa
Dev <devp2101@gmail.com>
2022-10-16 00:04:47 +0530

    -functions are refactors

--- frontend/controllers/ProfileController.php
+++ frontend/controllers/ProfileController.php
@@ -4,17 +4,35 @@ namespace frontend\controllers;
 
 use common\models\User;
 use yii\web\Controller;
+use yii\web\Response;
 
 class ProfileController extends Controller
 {
     /**
-     * @return string
+     * @return string|Response
      */
-    public function actionAccount(): string
+    public function actionAccount()
     {
         $user_id = \Yii::$app->user->identity->id;
         $model = User::findOne($user_id);
-        return $this->render('account');
+
+        if ($this->request->isPost) {
+            if ($model->load(\Yii::$app->request->post()) && $model->save()) {
+                \Yii::$app->session->setFlash('success','Profile Updated Successfully !');
+                return $this->refresh();
+            }
+        }
+        return $this->render('account',[
+            'model' => $model,
+        ]);
+    }
+
+    /**
+     * @return string
+     */
+    public function actionChangePassword(): string
+    {
+        return $this->render('change-password');
     }
 
 }
