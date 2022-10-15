c369de824c86475c49ae225958e5971b47dcb296
Dev <devp2101@gmail.com>
2022-10-16 00:05:04 +0530

    -new links are added in this files

--- frontend/views/profile/_partial/_profile_sidebar.php
+++ frontend/views/profile/_partial/_profile_sidebar.php
@@ -8,7 +8,12 @@ use yii\helpers\Url;
     <!-- Nav tabs -->
     <div class="dashboard_tab_button" data-aos="fade-up" data-aos-delay="0">
         <ul role="tablist" class="nav flex-column dashboard-list">
-            <li><a href="<?= Url::toRoute('profile/account')?>" data-bs-toggle="tab" class="nav-link active">Account Details</a></li>
+            <li>
+                <a href="<?= Url::toRoute('profile/account')?>" data-bs-toggle="tab" class="nav-link active">Account Details</a>
+            </li>
+            <li>
+                <a href="<?= Url::toRoute('profile/change-password')?>" data-bs-toggle="tab" class="nav-link">Change Password</a>
+            </li>
             <li> <a href="<?= Url::toRoute('profile/wishlist')?>" data-bs-toggle="tab" class="nav-link"> Wishlist</a></li>
             <li> <a href="<?= Url::toRoute('profile/order')?>" data-bs-toggle="tab" class="nav-link">My Order</a></li>
             <li>
