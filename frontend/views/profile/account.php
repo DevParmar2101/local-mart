ea70d7cb028cd1203f1f4b3b5df6dd057c636f61
Dev <devp2101@gmail.com>
2022-10-15 23:47:58 +0530

    -Bug fixes

--- frontend/views/profile/account.php
+++ frontend/views/profile/account.php
@@ -1,5 +1,10 @@
 <?php
 
+/** @var $model \common\models\User*/
+/** @var $form ActiveForm*/
+
+use yii\bootstrap5\ActiveForm;
+
 ?>
 <!-- account area start -->
 <div class="account-dashboard pt-100px pb-100px">
@@ -14,52 +19,13 @@
                         <div class="login">
                             <div class="login_form_container">
                                 <div class="account_login_form">
-                                    <form action="#">
-                                        <div class="input-radio">
-                                                    <span class="custom-radio"><input type="radio" value="1"
-                                                                                      name="id_gender"> Mr.</span>
-                                            <span class="custom-radio"><input type="radio" value="1"
-                                                                              name="id_gender"> Mrs.</span>
-                                        </div> <br>
-                                        <div class="default-form-box mb-20">
-                                            <label>First Name</label>
-                                            <input type="text" name="first-name">
+                                    <?php $form = ActiveForm::begin(['id' => 'profile-account-form']);?>
+                                        <div class="row">
+                                            <div class="col-md-6 col-12">
+
+                                            </div>
                                         </div>
-                                        <div class="default-form-box mb-20">
-                                            <label>Last Name</label>
-                                            <input type="text" name="last-name">
-                                        </div>
-                                        <div class="default-form-box mb-20">
-                                            <label>Email</label>
-                                            <input type="text" name="email-name">
-                                        </div>
-                                        <div class="default-form-box mb-20">
-                                            <label>Password</label>
-                                            <input type="password" name="user-password">
-                                        </div>
-                                        <div class="default-form-box mb-20">
-                                            <label>Birthdate</label>
-                                            <input type="date" name="birthday">
-                                        </div>
-                                        <span class="example">
-                                                (E.g.: 05/31/1970)
-                                            </span>
-                                        <br>
-                                        <label class="checkbox-default" for="offer">
-                                            <input type="checkbox" id="offer">
-                                            <span>Receive offers from our partners</span>
-                                        </label>
-                                        <br>
-                                        <label class="checkbox-default checkbox-default-more-text" for="newsletter">
-                                            <input type="checkbox" id="newsletter">
-                                            <span>Sign up for our newsletter<br><em>You may unsubscribe at any
-                                                        moment. For that purpose, please find our contact info in the
-                                                        legal notice.</em></span>
-                                        </label>
-                                        <div class="save_button mt-3">
-                                            <button class="btn" type="submit">Save</button>
-                                        </div>
-                                    </form>
+                                    <?php ActiveForm::end();?>
                                 </div>
                             </div>
                         </div>
