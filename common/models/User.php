f902cacdf599a4d670c5321cf8ca48710760ca44
Dev <devp2101@gmail.com>
2022-10-15 10:30:18 +0530

    -new attributes and new rules are added in User model and also added in user table

--- common/models/User.php
+++ common/models/User.php
@@ -22,6 +22,9 @@ use yii\web\IdentityInterface;
  * @property integer $created_at
  * @property integer $updated_at
  * @property string $password write-only password
+ * @property string $contact_number
+ * @property string $otp
+ * @property integer $otp_expire
  */
 class User extends ActiveRecord implements IdentityInterface
 {
@@ -56,6 +59,8 @@ class User extends ActiveRecord implements IdentityInterface
         return [
             ['status', 'default', 'value' => self::STATUS_INACTIVE],
             ['status', 'in', 'range' => [self::STATUS_ACTIVE, self::STATUS_INACTIVE, self::STATUS_DELETED]],
+            [['contact_number', 'otp'], 'string'],
+            [['otp_expire'], 'integer'],
         ];
     }
 
