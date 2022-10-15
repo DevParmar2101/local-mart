31d7f40cb51e12e654b32dc78a344461a609b7b4
Dev <devp2101@gmail.com>
2022-10-16 00:25:57 +0530

    -code is added in _profile_sidebar

---
+++ frontend/views/profile/change-password.php
@@ -0,0 +1,57 @@
+<?php
+
+/** @var $model \common\models\User*/
+/** @var $form ActiveForm*/
+
+use yii\bootstrap5\ActiveForm;
+use yii\helpers\Html;
+
+?>
+<!-- account area start -->
+<div class="account-dashboard pt-100px pb-100px">
+    <div class="container">
+        <div class="row">
+            <?= $this->render('_partial/_profile_sidebar')?>
+            <div class="col-sm-12 col-md-9 col-lg-9">
+                <!-- Tab panes -->
+                <div class="tab-content dashboard_content" data-aos="fade-up" data-aos-delay="200">
+                    <div class="tab-pane fade show active" id="account-details">
+                        <h3>Account details </h3>
+                        <div class="login">
+                            <div class="login_form_container">
+                                <div class="account_login_form">
+                                    <?php $form = ActiveForm::begin(['id' => 'profile-account-form']);?>
+
+                                    <div class="row">
+                                        <div class="col-md-8 col-12">
+                                            <?= $form->field($model,'old_password')->passwordInput()?>
+                                        </div>
+                                    </div>
+
+                                    <div class="row">
+                                        <div class="col-md-8 col-12">
+                                            <?= $form->field($model,'new_password')->passwordInput()?>
+                                        </div>
+                                    </div>
+
+                                    <div class="row">
+                                        <div class="col-md-8 col-12">
+                                            <?= $form->field($model,'confirm_password')->passwordInput()?>
+                                        </div>
+                                    </div>
+
+                                    <div class="place-order mt-25">
+                                        <?= Html::submitButton('Save',['class' => 'btn btn-primary blog-btn'])?>
+                                    </div>
+
+                                    <?php ActiveForm::end();?>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- account area start -->
