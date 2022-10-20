c15ef092201862f887060f79248e7f14a118b208
Dev <devp2101@gmail.com>
2022-10-20 17:51:46 +0530

    -UserStoreDocument model is generated in common\models folder

---
+++ common/models/UserStoreDocument.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "user_store_document".
+ *
+ * @property int $id
+ * @property string|null $document_name
+ * @property string|null $document
+ * @property int|null $status
+ * @property string|null $error_message
+ * @property int|null $document_verified_by
+ *
+ * @property User $documentVerifiedBy
+ */
+class UserStoreDocument extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'user_store_document';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['status', 'document_verified_by'], 'integer'],
+            [['document_name', 'document', 'error_message'], 'string', 'max' => 255],
+            [['document_verified_by'], 'exist', 'skipOnError' => true, 'targetClass' => User::class, 'targetAttribute' => ['document_verified_by' => 'id']],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'document_name' => 'Document Name',
+            'document' => 'Document',
+            'status' => 'Status',
+            'error_message' => 'Error Message',
+            'document_verified_by' => 'Document Verified By',
+        ];
+    }
+
+    /**
+     * Gets query for [[DocumentVerifiedBy]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getDocumentVerifiedBy()
+    {
+        return $this->hasOne(User::class, ['id' => 'document_verified_by']);
+    }
+}
