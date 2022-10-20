3e5ee3c0beab8cef8e4e882e6ef280dacb285522
Dev <devp2101@gmail.com>
2022-10-20 14:55:19 +0530

    -active class set dynamic and bug fix

--- frontend/views/layouts/seller-dashboard/left_sidebar.php
+++ frontend/views/layouts/seller-dashboard/left_sidebar.php
@@ -1,15 +1,21 @@
+<?php
+
+use common\components\BaseHelper;
+use yii\helpers\Url;
+
+?>
 
 <div class="sidebar-widget-group">
     <div class="sidebar-widget">
         <h3 class="sidebar-title">Seller's Dashboard</h3>
         <div class="category-post">
             <ul>
-                <li class="seller-dashboard active">
-                    <a href="#" class="selected m-0">Accesasories<span>(6)</span> </a>
+                <li class="seller-dashboard <?= BaseHelper::validateAction('site','index')?'active':''?>">
+                    <a href="<?= Url::toRoute('site/index')?>" class="selected m-0">Dashboard </a>
                 </li>
 
-                <li class="seller-dashboard">
-                    <a href="#" class="selected m-0">Accesasories<span>(6)</span> </a>
+                <li class="seller-dashboard <?= BaseHelper::validateAction('site','shop-list')?'active':''?>">
+                    <a href="<?= Url::toRoute('site/shop-list')?>" class="selected m-0">Shop List </a>
                 </li>
             </ul>
         </div>
