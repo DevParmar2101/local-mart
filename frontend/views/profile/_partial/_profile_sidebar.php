26d438e3c5c9cc43435f44412d514c4c530e37c0
Dev <devp2101@gmail.com>
2022-10-20 12:20:29 +0530

    - active class change to dynamic

--- frontend/views/profile/_partial/_profile_sidebar.php
+++ frontend/views/profile/_partial/_profile_sidebar.php
@@ -1,5 +1,6 @@
 <?php
 
+use common\components\BaseHelper;
 use yii\helpers\Html;
 use yii\helpers\Url;
 
@@ -9,13 +10,13 @@ use yii\helpers\Url;
     <div class="dashboard_tab_button" data-aos="fade-up" data-aos-delay="0">
         <ul role="tablist" class="nav flex-column dashboard-list">
             <li>
-                <a href="<?= Url::toRoute('profile/account')?>" class="nav-link active">Account Details</a>
+                <a href="<?= Url::toRoute('profile/account')?>" class="nav-link <?= BaseHelper::validateAction('profile','account') ?'active':'' ?>">Account Details</a>
             </li>
             <li>
-                <a href="<?= Url::toRoute('profile/change-password')?>" class="nav-link">Change Password</a>
+                <a href="<?= Url::toRoute('profile/change-password')?>" class="nav-link <?= BaseHelper::validateAction('profile','change-password') ?'active':'' ?>">Change Password</a>
             </li>
-            <li> <a href="<?= Url::toRoute('profile/wishlist')?>" class="nav-link"> Wishlist</a></li>
-            <li> <a href="<?= Url::toRoute('profile/order')?>" class="nav-link">My Order</a></li>
+            <li> <a href="<?= Url::toRoute('profile/wishlist')?>" class="nav-link <?= BaseHelper::validateAction('profile','wishlist') ?'active':'' ?>"> Wishlist</a></li>
+            <li> <a href="<?= Url::toRoute('profile/order')?>" class="nav-link <?= BaseHelper::validateAction('profile','order') ?'active':'' ?>">My Order</a></li>
             <li>
                 <?= Html::a('Logout',['site/logout'],['data' => ['method' => 'post'],'class' => 'nav-link'])?>
             </li>
