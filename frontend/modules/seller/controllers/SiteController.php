0f656020b7df781362d7ab8b43bff4e221b93ee3
Dev <devp2101@gmail.com>
2022-10-20 12:58:00 +0530

    - new code is added in site-controller file

---
+++ frontend/modules/seller/controllers/SiteController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace frontend\modules\seller\controllers;
+
+use yii\filters\AccessControl;
+use yii\filters\VerbFilter;
+use yii\web\Controller;
+
+class SiteController extends Controller
+{
+
+    /**
+     * @return array[]
+     */
+    public function behaviors()
+    {
+        return [
+            'access' => [
+                'class' => AccessControl::class,
+                'rules' => [
+                    [
+                        'actions' => ['login', 'error', 'signup', 'index'],
+                        'allow' => true,
+                    ],
+                ],
+            ],
+            'verbs' => [
+                'class' => VerbFilter::class,
+                'actions' => [
+                    'logout' => ['post'],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * @return \string[][]
+     */
+    public function actions()
+    {
+        return [
+            'error' => [
+                'class' => 'yii\web\ErrorAction',
+            ]
+        ];
+    }
+
+    public function actionIndex()
+    {
+        return $this->render('index');
+    }
+}
