70743e0faac15cb930b0b3914acbc66fd875e74b
Dev <devp2101@gmail.com>
2022-10-20 10:07:34 +0530

    -new migration file is generated for status table in states table

---
+++ console/migrations/m221020_043521_add_status_in_states_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use common\models\States;
+use yii\db\Migration;
+
+/**
+ * Class m221020_043521_add_status_in_states_table
+ */
+class m221020_043521_add_status_in_states_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m221020_043521_add_status_in_states_table cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m221020_043521_add_status_in_states_table cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
