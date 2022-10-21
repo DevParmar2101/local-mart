ea6261012d2891c9ad1b6c48300b3a637ef17fe4
Dev <devp2101@gmail.com>
2022-10-21 16:03:27 +0530

    - query is added in user_favourite table

--- console/migrations/m221021_095000_create_user_favourite_table.php
+++ console/migrations/m221021_095000_create_user_favourite_table.php
@@ -13,8 +13,13 @@ class m221021_095000_create_user_favourite_table extends Migration
     public function safeUp()
     {
         $this->createTable('{{%user_favourite}}', [
-            'id' => $this->primaryKey(),
+            'user_id' => $this->integer(11),
+            'product_id' => $this->integer(11),
         ]);
+
+        $this->addForeignKey('fk-User_id-User','user_favourite','user_id', \common\models\User::tableName(),'id');
+
+        $this->addForeignKey('fk-Product_id-UserFavourite','user_favourite','product_id', 'product','id');
     }
 
     /**
