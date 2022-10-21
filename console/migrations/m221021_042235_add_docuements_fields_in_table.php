b9934d23e00837e1a4e9fdba085611aba1cbde03
Dev <devp2101@gmail.com>
2022-10-21 10:08:18 +0530

    -add query in user_store table

--- console/migrations/m221021_042235_add_docuements_fields_in_table.php
+++ console/migrations/m221021_042235_add_docuements_fields_in_table.php
@@ -1,18 +1,25 @@
 <?php
 
+use common\models\UserStore;
 use yii\db\Migration;
 
 /**
- * Class m221021_042235_add_docuements_fields_in_table
+ * Class m221021_042235_add_documents_fields_in_table
  */
-class m221021_042235_add_docuements_fields_in_table extends Migration
+class m221021_042235_add_documents_fields_in_table extends Migration
 {
     /**
      * {@inheritdoc}
      */
     public function safeUp()
     {
+        $this->addColumn(UserStore::tableName(),'document_one',$this->string(255));
+        $this->addColumn(UserStore::tableName(),'document_one_verified',$this->tinyInteger(1));
+        $this->addColumn(UserStore::tableName(),'document_one_error_message',$this->string(255));
 
+        $this->addColumn(UserStore::tableName(),'document_two',$this->string(255));
+        $this->addColumn(UserStore::tableName(),'document_two_verified',$this->tinyInteger(1));
+        $this->addColumn(UserStore::tableName(),'document_two_error_message',$this->string(255));
     }
 
     /**
@@ -20,7 +27,7 @@ class m221021_042235_add_docuements_fields_in_table extends Migration
      */
     public function safeDown()
     {
-        echo "m221021_042235_add_docuements_fields_in_table cannot be reverted.\n";
+        echo "m221021_042235_add_documents_fields_in_table cannot be reverted.\n";
 
         return false;
     }
