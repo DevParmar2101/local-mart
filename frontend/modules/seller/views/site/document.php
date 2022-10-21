53896d01a81044ebac429beeca9f03763265612b
Dev <devp2101@gmail.com>
2022-10-21 14:25:24 +0530

    bug fix

--- frontend/modules/seller/views/site/document.php
+++ frontend/modules/seller/views/site/document.php
@@ -1,11 +1,12 @@
 <?php
 
-use common\widgets\DynamicFormWidget;
+use kartik\file\FileInput;
 use yii\bootstrap5\ActiveForm;
 use yii\helpers\Html;
 
 $this->title = 'Document Form';
 /** @var $form ActiveForm*/
+/** @var $model \common\models\UserStore*/
 
 ?>
 
@@ -13,93 +14,52 @@ $this->title = 'Document Form';
     <div class="col-md-12">
         <div class="tab-content dashboard_content" data-aos="fade-up" data-aos-delay="200">
             <div class="tab-pane fade show active">
-                <?php $form = ActiveForm::begin(['id' => 'dynamic-form']);?>
-                    <?php DynamicFormWidget::begin([
-                            'widgetContainer' => 'dynamicform_wrapper',
-                            'widgetBody' => '.container-items',
-                            'widgetItem' => '.item',
-                            'limit' => 4,
-                            'min' => 0,
-                            'insertButton' => '.add-item',
-                            'deleteButton' => '.remove-item',
-                            'model' => $modelsAddress[0],
-                            'formId' => 'dynamic-form',
-                            'formFields' => [
-                                    'document_name',
-                                    'document',
+                <h4>Documents : <?= $model->store_name?></h4>
+                <?php $form = ActiveForm::begin(['id' => 'user-store-document-form']); ?>
+                <div class="row">
+                    <div class="col-md-6 col-12">
+                        <?= $form->field($model,'document_one')->widget(FileInput::class,[
+                            'options' => [
+                                'accept' => 'image/*',
+                                'multiple' => false,
                             ],
-                ])?>
-                <div class="panel panel-default">
-                    <div class="panel-heading">
-                        <h4>
-                            <i class="glyphicon glyphicon-envelope"></i> Addresses
-                            <button type="button" class="add-item btn btn-success btn-sm pull-right"><i class="glyphicon glyphicon-plus"></i> Add</button>
-                        </h4>
+                            'pluginOptions' => [
+                                'initialPreview' => $model->document_one ? $model->getImage('document_one','Document One','img-thumbnail') : false,
+                                'initialPreviewData' => true,
+                                'browseClass' => 'btn btn-warning blog-btn',
+                                'showPreview' => true,
+                                'showCaption' => false,
+                                'showRemove' => false,
+                                'showCancel' => false,
+                                'showUpload' => false,
+                            ],
+                        ])?>
                     </div>
-                    <div class="panel-body">
-                        <div class="container-items">
-                            <?php foreach ($modelsAddress as $i => $modelAddress): ?>
-                                <div class="item panel panel-default">
-                                    <div class="panel-heading">
-                                        <h3 class="panel-title pull-left">Address</h3>
-                                        <div class="pull-right">
-                                            <button type="button" class="remove-item btn btn-danger btn-xs"><i class="glyphicon glyphicon-minus"></i></button>
-                                        </div>
-                                        <div class="clearfix"></div>
-                                    </div>
-                                    <div class="panel-body">
-                                        <?php
-                                            if (! $modelAddress->isNewRecord) {
-                                                echo \yii\helpers\Html::activeHiddenInput($modelAddress,"[{$i}]id");
-                                            }
-                                        ?>
-                                        <div class="row">
-                                            <div class="col-md-6 col-12">
-                                                <?= $form->field($modelAddress,"[{$i}]document_name")->textInput()?>
-                                            </div>
-                                            <div class="col-md-6 col-12">
-                                                <?= $form->field($modelAddress,"[{$i}]document")->textInput()?>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            <?php endforeach;?>
-                        </div>
+                    <div class="col-md-6 col-12">
+                        <?= $form->field($model,'document_two')->widget(FileInput::class,[
+                            'options' => [
+                                'accept' => 'image/*',
+                                'multiple' => true,
+                            ],
+                            'pluginOptions' => [
+                                'initialPreview' => $model->document_two ? $model->getImage('document_two','Document Two','img-thumbnail') : false,
+                                'initialPreviewData' => true,
+                                'browseClass' => 'btn btn-warning blog-btn',
+                                'showPreview' => true,
+                                'showCaption' => false,
+                                'showRemove' => false,
+                                'showCancel' => false,
+                                'showUpload' => false,
+                            ],
+                        ])?>
                     </div>
                 </div>
-                <?php DynamicFormWidget::end(); ?>
+
                 <div class="form-group">
-                    <?= Html::submitButton($modelAddress->isNewRecord ? 'Create' : 'Update', ['class' => 'btn btn-primary']) ?>
+                    <?= Html::submitButton('Save',['class' => 'btn btn-primary blog-btn'])?>
                 </div>
-                <?php ActiveForm::end();?>
+                <?php ActiveForm::end(); ?>
             </div>
         </div>
     </div>
 </div>
-<?php
-$js =<<<JS
-$(".dynamicform_wrapper").on("beforeInsert", function(e, item) {
-    console.log("beforeInsert");
-});
-
-$(".dynamicform_wrapper").on("afterInsert", function(e, item) {
-    console.log("afterInsert");
-});
-
-$(".dynamicform_wrapper").on("beforeDelete", function(e, item) {
-    if (! confirm("Are you sure you want to delete this item?")) {
-        return false;
-    }
-    return true;
-});
-
-$(".dynamicform_wrapper").on("afterDelete", function(e) {
-    console.log("Deleted item!");
-});
-
-$(".dynamicform_wrapper").on("limitReached", function(e, item) {
-    alert("Limit reached");
-});
-JS;
-$this->registerJs($js);
-?>
