97e44820963976ddab9b1c59745374a610ed42cf
Dev <devp2101@gmail.com>
2022-10-21 15:18:09 +0530

    -query is added in this migration file

--- console/migrations/m221021_093638_create_product_table.php
+++ console/migrations/m221021_093638_create_product_table.php
@@ -14,6 +14,20 @@ class m221021_093638_create_product_table extends Migration
     {
         $this->createTable('{{%product}}', [
             'id' => $this->primaryKey(),
+            'store_id' => $this->integer(),
+            'product_name' => $this->string(255),
+            'category' => $this->integer(11),
+            'sub_category' => $this->integer(11),
+            'product_price' => $this->float(10.15),
+            'discount' => $this->float(10.15),
+            'quantity' => $this->integer(11),
+            'information' => $this->getDb()->getSchema()->createColumnSchemaBuilder('longtext'),
+            'description' => $this->getDb()->getSchema()->createColumnSchemaBuilder('longtext'),
+            'warranty_period' => $this->string(255),
+            'available_for' => $this->tinyInteger(2),
+            'status' => $this->tinyInteger(2),
+            'created_at' => $this->dateTime(),
+            'updated_at' => $this->dateTime(),
         ]);
     }
 
