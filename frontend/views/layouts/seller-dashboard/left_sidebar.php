2cc9dc96a0d75a0fbdb2b0aa072f0be7756e0519
Dev <devp2101@gmail.com>
2022-10-22 15:00:42 +0530

    - new link is added in seller sidebar

--- frontend/views/layouts/seller-dashboard/left_sidebar.php
+++ frontend/views/layouts/seller-dashboard/left_sidebar.php
@@ -10,6 +10,7 @@ use yii\helpers\Url;
         <h3 class="sidebar-title">Seller's Dashboard</h3>
         <div class="category-post">
             <ul>
+
                 <li class="seller-dashboard <?= BaseHelper::validateAction('site','index')?'active':''?>">
                     <a href="<?= Url::toRoute('site/index')?>" class="selected m-0">Dashboard </a>
                 </li>
@@ -17,6 +18,11 @@ use yii\helpers\Url;
                 <li class="seller-dashboard <?= BaseHelper::validateAction('site','shop-list')?'active':''?>">
                     <a href="<?= Url::toRoute('site/shop-list')?>" class="selected m-0">Shop List </a>
                 </li>
+
+                <li class="seller-dashboard <?= BaseHelper::validateAction('product','index')?'active':''?>">
+                    <a href="<?= Url::toRoute('product/index')?>" class="selected m-0">Product List </a>
+                </li>
+
             </ul>
         </div>
     </div>
