5a28eb9eb785c898079aad21b3741708509bf04e
Dev <devp2101@gmail.com>
2022-11-02 11:42:35 +0530

    - new bug fix in this files

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -18,7 +18,7 @@ AppAsset::register($this);
         <meta charset="<?= Yii::$app->charset ?>">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <?php $this->registerCsrfMetaTags() ?>
-        <title>Local Mart |<?= Html::encode($this->title) ?></title>
+        <title>Local Mart | <?= Html::encode($this->title) ?></title>
         <?php $this->head() ?>
     </head>
     <body>
