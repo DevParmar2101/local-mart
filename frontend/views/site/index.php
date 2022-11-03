36cab32c4a40516953f6ddc7a7ff8cf10b26e95e
Dev <devp2101@gmail.com>
2022-11-03 17:52:24 +0530

    - new _partial view file is generated

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -57,27 +57,9 @@ $this->title = 'My Yii Application';
 </div>
 <!-- Hero/Intro Slider End -->
 <!-- Banner Area Start -->
-<div class="banner-area style-one pt-100px pb-100px">
-    <div class="container">
-        <div class="row">
-            <?php foreach ($sub_categories as $category) {
-                /** @var $category StoreSubCategory*/
-                ?>
-            <div class="col-md-6">
-                <div class="single-banner nth-child-2 mb-30px mb-lm-30px mt-lm-30px is_featured_image">
-                    <?= $category->getImage('image','Not Found','')?>
-<!--                    <img src="assets/images/banner/4.webp" alt="">-->
-                    <div class="banner-content nth-child-2">
-                        <h3 class="title"><?= $category->sub_category?></h3>
-                        <span class="category">From $95.00</span>
-                        <a href="shop-left-sidebar.html" class="shop-link"><i class="fa fa-arrow-right" aria-hidden="true"></i></a>
-                    </div>
-                </div>
-            </div>
-            <?php }?>
-        </div>
-    </div>
-</div>
+<?= $this->render('_partial/_featured_product_sub_category',[
+        'sub_categories' => $sub_categories
+]);?>
 <!-- Banner Area End -->
 <!-- Product Area Start -->
 <div class="product-area pb-100px">
