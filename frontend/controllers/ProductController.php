5ac7359c4f98ecb7319e4d7889c9552440b604f7
Dev <devp2101@gmail.com>
2022-11-11 12:39:20 +0530

    - bug fixes

--- frontend/controllers/ProductController.php
+++ frontend/controllers/ProductController.php
@@ -3,6 +3,7 @@
 namespace frontend\controllers;
 
 use common\models\Product;
+use common\models\ProductImage;
 use yii\web\Controller;
 
 class ProductController extends Controller
@@ -11,9 +12,11 @@ class ProductController extends Controller
     public function actionIndex($uuid)
     {
         $model = Product::findOne(['uuid' => $uuid]);
+        $product_image = ProductImage::find()->where(['product_id' => $model->id])->all();
 
         return $this->render('index',[
             'model' => $model,
+            'product_image' => $product_image,
         ]);
     }
 }
